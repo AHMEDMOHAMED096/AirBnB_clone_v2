4fc27ae93eff23fbde9ccfead745acc3d65ed9ea
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-05-16 17:15:18 +0300

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ service nginx start
 
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-touch /data/web_static/releases/test/index.html
+touch /data/web_static/releases/test/index.html ; echo "Testing for deployment" >> /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
 
@@ -17,7 +17,7 @@ echo "server {
     listen [::]:80 default_server;
 
     location /hbnb_static {
-        alias /data/web_static/current
+        alias /data/web_static/current;
     }
 }" > /etc/nginx/sites-available/default
 
