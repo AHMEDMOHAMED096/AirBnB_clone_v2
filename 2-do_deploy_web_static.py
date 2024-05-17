f0e3ed646b5aaa563207a8d66858e706d1c92914
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-05-17 07:55:44 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,7 +18,7 @@ def do_deploy(archive_path):
     run("mkdir -p /data/web_static/releases/{}".format(archive_filename))
 
     with cd("/data/web_static/releases/{}".format(archive_filename)):
-        run("tar -xzf /tmp/{} -C /".format(os.path.basename(archive_path)))
+        run("tar -xzf /tmp/{} -C .".format(os.path.basename(archive_path)))
 
     run("rm -rf /tmp/{}".format(os.path.basename(archive_path)))
 
