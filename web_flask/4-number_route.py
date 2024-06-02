0237241a948fd45e26d180fdfa15b2d3615f5aa2
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-06-03 02:05:47 +0300

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    """Displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def display_text(text):
+    """Displays 'C ' followed by the value of the text variable"""
+    modified_text = text.replace("_", " ")
+    return f"C {modified_text}"
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def display_P_text(text="is cool"):
+    """Displays 'Python ' followed by the value of the text variable"""
+    modified_text = text.replace("_", " ")
+    return f"Python {modified_text}"
+
+
+@app.route("/number/<n>")
+def display_number(n):
+    """Displays the value of n if it is an integer only"""
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
